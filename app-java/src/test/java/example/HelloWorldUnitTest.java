8840fc3dc054c8901d6ab834d5c485ba96ba2e47
dbouman <denniebouman@gmail.com>
2020-10-06 19:27:45 +0200

    change groupId, fix failing unit test

--- app-java/src/test/java/example/HelloWorldUnitTest.java
+++ app-java/src/test/java/example/HelloWorldUnitTest.java
@@ -19,7 +19,7 @@ public class HelloWorldUnitTest {
     @Test
     public void test3() {
         String result = new HelloWorld().getWords(5);
-        assertThat(result, equalTo("four"));
+        assertThat(result, equalTo("five"));
     }
 
 }
